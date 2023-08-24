80c79ffabed78da76ceb977b400c4e4bd6307502
Lucas Cubile <cubilelucas@gmail.com>
2023-08-24 15:18:35 -0300

    add gallery

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$(document).ready(() => {
+  console.log('OK TO CODE')
+
+  $('.galeria').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1200,
+    resposive: true,
+  })
+})
